e85ddf87881f076964ee2990abc6789287f827fa
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-03-19 09:57:55 +0530

    Update Check whether BST has Dead Ends.py

--- BST/Check whether BST has Dead Ends.py
+++ BST/Check whether BST has Dead Ends.py
@@ -1,6 +1,7 @@
 def checkBST(root,mini,maxi):
     if root==None:
         return True
+    
     if maxi-mini == 0:
         return False
     return checkBST(root.left,mini,root.data-1) and checkBST(root.right,root.data+1,maxi)
