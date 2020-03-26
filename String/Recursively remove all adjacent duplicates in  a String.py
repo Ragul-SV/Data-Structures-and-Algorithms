227406d0814347c4b0edaab43ccf5e45cc471f3f
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-03-26 11:15:07 +0530

    Update Recursively remove all adjacent duplicates in  a String.py

--- String/Recursively remove all adjacent duplicates in  a String.py
+++ String/Recursively remove all adjacent duplicates in  a String.py
@@ -1,6 +1,13 @@
 def duplicate(st,i,res):
     if i==len(st):
-        return res
+        flag = True
+        for j in range(len(res)-1):
+            if res[j]==res[j+1]:
+                flag = False
+        if flag:
+            return res
+        else:
+            return duplicate(res,0,"")
     if ((i+1)<=(len(st)-1) and st[i]==st[i+1]) or ((i-1)>=0 and st[i]==st[i-1]):
         return duplicate(st,i+1,res)
     else:
@@ -11,21 +18,4 @@ t = int(input())
 for cases in range(t):
     st = input()
     res = ""
-    flag = True
-    for j in range(len(st)-1):
-        if st[j]==st[j+1]:
-            flag = False
-    if flag==True:
-        print(st)
-    while flag==False:
-        res = duplicate(st,0,res)
-        flag = True
-        for j in range(len(res)-1):
-            if res[j]==res[j+1]:
-                flag = False
-        if flag==True:
-            print(res)
-            break
-        else:
-            st = str(res)
-            res = ""
+    print(duplicate(st,0,res))
