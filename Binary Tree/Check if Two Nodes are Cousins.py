4ac0f45cf883da4f936686ada51e31e2a34c9acc
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-03-27 23:47:32 +0530

    Update Check if Two Nodes are Cousins.py

--- Binary Tree/Check if Two Nodes are Cousins.py
+++ Binary Tree/Check if Two Nodes are Cousins.py
@@ -3,13 +3,9 @@ def vertical(root,value,level,depth,arr):
         if root.left and root.left.data == value:
             arr.append(level+1)
             arr.append(root)
-            arr.append("L")
-            arr.append(root.left)
         elif root.right and root.right.data == value:
             arr.append(level+1)
             arr.append(root)
-            arr.append("R")
-            arr.append(root.right)
         vertical(root.left,value,level+1,depth-1,arr)
         vertical(root.right,value,level+1,depth+1,arr)
 
@@ -18,8 +14,6 @@ def isCousin(root,a,b):
     vertical(root,a,0,0,arr1)
     arr2 = []
     vertical(root,b,0,0,arr2)
-    # print(arr1[0],arr1[1].data,arr1[2],arr1[3].data)
-    # print(arr2[0],arr2[1].data,arr2[2],arr2[3].data)
     if not arr1 or not arr2:
         return False
     elif arr1[0]==arr2[0]:
