bc7244363c183828902f336978343a9362696a63
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-03-28 08:48:07 +0530

    Create Fold-able Binary Tree.py

---
+++ Fold-able Binary Tree.py
@@ -0,0 +1,31 @@
+def IsFoldable(root):
+    if not root:
+        return True
+    q =  [root]
+    curnode = q.pop(0)
+    if not curnode.left and curnode.right:
+        return False
+    elif not curnode.right and curnode.left:
+        return False
+    elif curnode.left and curnode.right:
+        q.append(curnode.left)
+        q.append(curnode.right)
+        while q:
+            l = q.pop(0)
+            r = q.pop(0)
+            if not l.left and not r.right and not l.right and not r.left:
+                continue
+            elif l.left and r.right and not l.right and not r.left:
+                q.append(l.left)
+                q.append(r.right)
+            elif not l.left and not r.right and l.right and r.left:
+                q.append(l.right)
+                q.append(r.left)
+            elif l.left and r.right and l.right and r.left:
+                q.append(l.left)
+                q.append(r.right)
+                q.append(l.right)
+                q.append(r.left)
+            else:
+                return False
+    return True
