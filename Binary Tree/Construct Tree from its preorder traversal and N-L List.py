6b8b73d2c7cb5141b507cd3ac6b9aacbfdbdc56c
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-03-29 12:34:50 +0530

    Create Construct Tree from its preorder traversal and N-L List.py

---
+++ Binary Tree/Construct Tree from its preorder traversal and N-L List.py
@@ -0,0 +1,15 @@
+def construct(pre,preLN,n,index):
+    if index[0]>=n:
+        return
+    if preLN[index[0]]=='L':
+        return Node(pre[index[0]])
+    root = Node(pre[index[0]])
+    index[0]+=1
+    root.left = construct(pre,preLN,n,index)
+    index[0]+=1
+    root.right = construct(pre,preLN,n,index)
+    return root
+    
+def constructTree(pre, preLN, n):
+    index = [0]
+    return construct(pre,preLN,n,index)
