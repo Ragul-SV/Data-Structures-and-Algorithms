7af84a5d78b6b54133ab242ac60c4d99d3629b91
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-04-05 15:52:23 +0530

    Create Smallest Distinct Window.py
    Important

---
+++ Hash Map/Medium/Smallest Distinct Window.py
@@ -0,0 +1,50 @@
+def smallestWindow(s,p):
+    lens = len(s)
+    lenp = len(p)
+    if lens<lenp:
+        return -1
+    dp = dict()
+    for i in range(lenp):
+        if p[i] not in dp:
+            dp[p[i]] = 1
+        else:
+            dp[p[i]] += 1
+          
+    start = 0
+    start_index = -1
+    min_length = 2**31
+    ds = dict()
+    c = 0
+    for i in range(lens):
+        if s[i] not in ds:
+            ds[s[i]] = 1
+        else:
+            ds[s[i]] += 1
+    
+        if s[i] in dp and ds[s[i]]<=dp[s[i]]:
+            c+=1
+        if c==lenp:
+            while s[start] not in dp or ds[s[start]]>dp[s[start]]:
+                if s[start] in dp and ds[s[start]]>dp[s[start]]:
+                    ds[s[start]]-=1
+                start += 1
+                
+            len_window = i-start+1
+            if min_length>len_window:
+                min_length = len_window
+                start_index = start
+                
+    if(start_index==-1):
+        return -1
+    return min_length
+
+t = int(input())
+for cases in range(t):
+    s = input()
+    d = dict()
+    for i in range(len(s)):
+        if s[i] not in d:
+            d[s[i]] = 1
+    p = list(d.keys())
+    p = "".join(p)
+    print(smallestWindow(s,p))
