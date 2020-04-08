37694a361af281eb5300b189bf4b81874e5628e6
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-04-08 10:42:04 +0530

    Update Missing number Binary Search.py

--- Array/Easy/Missing number Binary Search.py
+++ Array/Easy/Missing number Binary Search.py
@@ -15,3 +15,14 @@ for cases in range(t):
         s+=i
         s-=arr[i-2]
     print(s)
+#---------------------------------------------METHOD3-----------------------------------------------#
+t = int(input())
+for cases in range(t):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    s1,s2 = 0,0
+    for i in range(1,n+1):
+        s1 = s1^i
+    for i in range(n-1):
+        s2 = s2^arr[i]
+    print(s1^s2)
