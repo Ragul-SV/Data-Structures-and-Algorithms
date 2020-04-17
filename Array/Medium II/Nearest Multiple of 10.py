8c4a750c6f3f1ba4922f78a807e92ef3fdee3458
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-04-17 11:56:36 +0530

    Create Nearest Multiple of 10.py

---
+++ Array/Medium II/Nearest Multiple of 10.py
@@ -0,0 +1,23 @@
+t = int(input())
+for cases in range(t):
+    s = input()
+    k = len(s)-1
+    if int(s[k])==0:
+        print(s)
+    elif int(s[k])<=5:
+        s = s[:k]+'0'+s[k+1:]
+        print(s)
+    elif int(s[k])>5:
+        s = s[:k]+'0'+s[k+1:]
+        flag = 1
+        while k>=1 and flag==1:
+            k-=1
+            if int(s[k])<9:
+                s = s[:k]+str(int(s[k])+1)+s[k+1:]
+                print(s)
+                flag = 0
+            else:
+                s = s[:k]+'0'+s[k+1:]
+        if k==0 and flag==1:
+            s = '1'+s
+            print(s)
