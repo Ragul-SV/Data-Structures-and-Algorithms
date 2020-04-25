6f5e65435f7c9808ad9851651cddc0cf262f8f9c
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-04-25 10:44:53 +0530

    Update Inversion of array.py

--- Array/Medium/Inversion of array.py
+++ Array/Medium/Inversion of array.py
@@ -1,37 +1,50 @@
-def mergeSort(arr,temp,l,r):
-	if l<r:
-		mid = (l+r)//2
-		mergeSort(arr,temp,l,mid)
-		mergeSort(arr,temp,mid+1,r)
-		merge(arr,temp,l,mid,r)
-		
-def merge(arr,temp,l,mid,r):
-	i = l
-	j = mid+1
-	k = l
-	while i<=mid and j<=r:
-		if arr[i]<=arr[j]:
-			temp[k]=arr[i]
-			i+=1
-		else:
-			temp[k]=arr[j]
-			j+=1
-		k+=1
-	while i<=mid:
-		temp[k]=arr[i]
-		k+=1
-		i+=1
-	while j<=r:
-		temp[k]=arr[j]
-		k+=1
-		j+=1
-	for i in range(l,r+1):
-		arr[i] = temp[i]
+def mergeSort(arr,n):
+    temp = [0]*n
+    return mergeSortUtil(arr,temp,0,n-1)
+    
+def mergeSortUtil(arr,temp,left,right):
+    inv_count = 0
+    if left<right:
+        mid = (left+right)//2
+        inv_count += mergeSortUtil(arr,temp,left,mid)
+        inv_count += mergeSortUtil(arr,temp,mid+1,right)
+        inv_count += merge(arr,temp,left,mid,right)
+    return inv_count
+
+def merge(arr,temp,left,mid,right):
+    i = left
+    j = mid+1
+    k = left
+    inv_count = 0
+    
+    while i<=mid and j<=right:
+        if arr[i]<=arr[j]:
+            temp[k] = arr[i]
+            k+=1
+            i+=1
+        else:
+            temp[k] = arr[j]
+            inv_count += (mid-i)+1
+            k+=1
+            j+=1
+            
+    while i<=mid:
+        temp[k] = arr[i]
+        k+=1
+        i+=1
+        
+    while j<=right:
+        temp[k] = arr[j]
+        k+=1
+        j+=1
+    
+    for i in range(left,right+1):
+        arr[i] = temp[i]
+        
+    return inv_count
 
 t = int(input())
 for cases in range(t):
-	n = int(input())
-	arr = list(map(int,input().strip().split()))
-	temp = [0]*n
-	mergeSort(arr,temp,0,n-1)
-	print(arr)
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    print(mergeSort(arr,n))
