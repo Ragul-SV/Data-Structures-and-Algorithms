de7367c0cf4ff33d394b8d24dbe84394ada05643
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-15 11:25:27 +0530

    Update Check if Subtree.py
    Check if a binary tree T is subtree of another binary tree S

--- Binary Tree/Check if Subtree.py
+++ Binary Tree/Check if Subtree.py
@@ -1,3 +1,4 @@
+#----------------------------------------------O(M*N)-----------------------------------------------------------------------------#
 def isSame(root1,root2):
     if not root1 and not root2:
         return True
@@ -15,3 +16,43 @@ def isSubTree(root1, root2):
     l = isSubTree(root1.left,root2)
     r = isSubTree(root1.right,root2)
     return isSame(root1,root2) or l or r
+#--------------------------------------------------O(N)-----------------------------------------------------------------------#
+class Node:
+	def __init__(self,value):
+		self.data = value
+		self.left = self.right = None
+	
+def storeinorder(root,arr):
+	if not root:
+		arr.append('$')
+	storeinorder(root.left,arr)
+	arr.append(root.data)
+	storeinorder(root.right,arr)
+	
+def storepreorder(root,arr):
+	if not root:
+		arr.append('$')
+	arr.append(root.data)
+	storepreorder(root.left,arr)
+	storepreorder(root.right,arr)	
+	
+def isSubtree(T,S):
+	if not S:
+		return True
+	if not T:
+		return False
+	inT = []
+	inS = []
+	storeinorder(T,inT)
+	storeinorder(S,inS)
+	inT = "".join(inT)
+	inS = "".join(inS)
+	if inT not in inS:
+		return False
+	preT = []
+	preS = []
+	storepreorder(T,preT)
+	storepreorder(S,preS)
+	preT = "".join(preT)
+	preS = "".join(preS)
+	return preT in preS
