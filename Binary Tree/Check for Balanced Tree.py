511c26cf54ca3b8f4c5f60b6c6d6ac9583b549ae
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-15 09:07:28 +0530

    Update Check for Balanced Tree.py

--- Binary Tree/Check for Balanced Tree.py
+++ Binary Tree/Check for Balanced Tree.py
@@ -1,3 +1,4 @@
+#----------------------METHOD1-----------------O(N^2)-------------------------------------------------------------------#
 def height(root):
     if not root:
         return 0
@@ -7,3 +8,20 @@ def isBalanced(root):
     if not root:
         return True
     return (abs(height(root.left)-height(root.right))<=1) and isBalanced(root.left) and isBalanced(root.right) 
+#----------------------METHOD2-----------------O(N)---------------------------------------------------------------------#
+def dfsheight(root):
+    if not root:
+        return 0
+    lh = dfsheight(root.left)
+    if lh == -1:
+        return -1
+    rh = dfsheight(root.right)
+    if rh == -1:
+        return -1
+    if abs(lh-rh)>1:
+        return -1
+    return 1 + max(lh,rh)
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return dfsheight(root)!=-1
