894e4233b07246afa13d6dfab7bc3edc16770287
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-16 12:27:19 +0530

    Update Extreme nodes in alternate order.py

--- Binary Tree/Extreme nodes in alternate order.py
+++ Binary Tree/Extreme nodes in alternate order.py
@@ -1,23 +1,15 @@
-def alternate(root,level,d,flag):
+def alternate(root,level,D):
     if root:
-        if level%2==0 and flag[level]:
-            try:
-                d[level].append(root.data)
-            except:
-                d[level] = [root.data]
-        else:
-            try:
-                d[level].append(root.data)
-            except:
-                d[level] = [root.data]
-        alternate(root.left,level+1,d,flag)
-        alternate(root.right,level+1,d,flag)
+            d[level].append(root.data)
+        except:
+            d[level] = [root.data]
+        alternate(root.left,level+1,d)
+        alternate(root.right,level+1,d)
 
 def printExtremeNodes(root):
     # Code here
     d = dict()
-    flag = [True]*100
-    alternate(root,0,d,flag)
+    alternate(root,0,d)
     for i,value in enumerate(d.values()):
         if i%2==0:
             print(value[-1],end=" ")
