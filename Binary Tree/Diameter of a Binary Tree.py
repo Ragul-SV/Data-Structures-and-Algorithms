d430fa799c8b585fc38198a918ce2fd24911aa6d
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-16 12:14:22 +0530

    Update Diameter of a Binary Tree.py

--- Binary Tree/Diameter of a Binary Tree.py
+++ Binary Tree/Diameter of a Binary Tree.py
@@ -1,3 +1,4 @@
+#--------------------------------------TOP DOWN APPROACH------------O(N^2)----------------------------------------------#
 def height(root):
     if not root:
         return 0
@@ -7,3 +8,22 @@ def diameter(root):
     if not root:
         return 0
     return max(diameter(root.left),diameter(root.right),1+height(root.left)+height(root.right))
+#--------------------------------------BOTTOM UP APPROACH------------O(N)----------------------------------------------#
+class Height:
+    def __init__(self):
+        self.h = 0
+        
+def diameterUtil(root,height):
+    lh = Height()
+    rh = Height()
+    if not root:
+        height.h = 0
+        return 0
+    ldiameter = diameterUtil(root.left,lh)
+    rdiameter = diameterUtil(root.right,rh)
+    height.h = 1 + max(lh.h,rh.h)
+    return max(1+lh.h+rh.h,max(ldiameter,rdiameter))
+    
+def diameter(root):
+    height = Height()
+    return diameterUtil(root,height)
