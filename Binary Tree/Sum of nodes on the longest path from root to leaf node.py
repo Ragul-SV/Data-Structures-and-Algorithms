792784503cd1d80f6bdbd0c687928e95c9e50732
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-17 13:08:38 +0530

    Update Sum of nodes on the longest path from root to leaf node.py

--- Binary Tree/Sum of nodes on the longest path from root to leaf node.py
+++ Binary Tree/Sum of nodes on the longest path from root to leaf node.py
@@ -1,3 +1,4 @@
+#---------------TOP DOWN APPROACH-----------------------O(N^2)------------------------------------------------------------------------#
 def height(root):
     if not root:
         return 0
@@ -11,3 +12,22 @@ def sumOfLongRootToLeafPath(root):
             return root.data + sumOfLongRootToLeafPath(root.right)
         return root.data + max(sumOfLongRootToLeafPath(root.left),sumOfLongRootToLeafPath(root.right))
     return 0
+#------------------BOTTOM UP APPROACH---------------------O(N)------------------------------------------------------------------------#
+def sumOfLongRootToLeafPathUtil(root,s,l,maxs,maxl):
+    if not root:
+        if l>maxl[0]:
+            maxl[0] = l
+            maxs[0] = s
+        elif l==maxl[0] and s>maxs[0]:
+            maxs[0] = s
+        return
+    sumOfLongRootToLeafPathUtil(root.left,s+root.data,l+1,maxs,maxl)
+    sumOfLongRootToLeafPathUtil(root.right,s+root.data,l+1,maxs,maxl)
+    
+def sumOfLongRootToLeafPath(root):
+    if not root:
+        return 0
+    maxs = [-2**31]
+    maxl = [0]
+    sumOfLongRootToLeafPathUtil(root,0,0,maxs,maxl)
+    return maxs[0]
