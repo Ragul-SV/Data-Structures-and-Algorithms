dbe8bc3079683f2b3c23db90620ddff703d5663d
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-17 11:44:28 +0530

    Delete Maximum diameter in a Binary Tree.py

--- Binary Tree/Maximum diameter in a Binary Tree.py
+++
@@ -1,9 +0,0 @@
-def height(root):
-    if not root:
-        return 0
-    return 1 + max(height(root.left),height(root.right))
-
-def diameter(root):
-    if not root:
-        return 0
-    return max(diameter(root.left),diameter(root.right),1+height(root.left)+height(root.right))
