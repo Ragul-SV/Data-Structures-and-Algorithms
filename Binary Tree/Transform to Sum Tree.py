9d40f15f0033c1c5a3afc0ebff62cca330a8ae36
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-05-26 10:55:52 +0530

    Create Transform to Sum Tree.py

--- Binary Tree/Transform to Sum Tree.py
+++ Binary Tree/Transform to Sum Tree.py
@@ -1,8 +1,6 @@
 def toSumTree(root) :
     if not root:
         return 0
-    curdata = root.data
     l = toSumTree(root.left)
     r = toSumTree(root.right)
-    root.data = l+r
-    return root.data + curdata
+    return l + r + root.data
