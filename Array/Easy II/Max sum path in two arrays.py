7b87e73eac314f76033ca916df4936a81abaaac7
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-07-04 14:40:13 +0530

    Update Max sum path in two arrays.py

--- Array/Easy II/Max sum path in two arrays.py
+++ Array/Easy II/Max sum path in two arrays.py
@@ -5,27 +5,24 @@ def maxSumPath(arr1, arr2, m, n):
     res = 0
     while i<m and j<n:
         if arr1[i]==arr2[j]:
-            res += max(s1,s2)+arr1[i]
+            res += max(s1,s2)
             s1 = 0
             s2 = 0
-        else:
-            s1+=arr1[i]
-            s2+=arr2[j]
-        i+=1
-        j+=1
-        if i==m and j==n:
-            res+=max(s1,s2)
-        elif i==m:
-            temp = s2
-            while j<n:
-                temp+=arr2[j]
-                j+=1
-            res+=max(s1,temp)
-        elif j==n:
-            temp = s1
-            while i<m:
-                temp+=arr1[i]
+            while i<m and j<n and arr1[i]==arr2[j]:
+                res+=arr1[i]
                 i+=1
-            res+=max(s2,temp)
-        print(i,res)
+                j+=1
+        elif arr1[i]<arr2[j]:
+            s1+=arr1[i]
+            i+=1
+        elif arr2[j]<arr1[i]:
+            s2+=arr2[j]
+            j+=1
+    while i<m:
+        s1+=arr1[i]
+        i+=1
+    while j<n:
+        s2+=arr2[j]
+        j+=1
+    res+=max(s1,s2)
     return res
