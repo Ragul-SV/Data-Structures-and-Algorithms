240d204b44acfd6d06013f8bfaa98d33db194ab9
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-07-06 16:11:26 +0530

    Create Subarray with given Sum(Positive integers).py

--- Array/Easy/Subarray with given Sum(Positive integers).py
+++ Array/Easy/Subarray with given Sum(Positive integers).py
@@ -3,17 +3,17 @@ for cases in range(t):
     n,k = map(int,input().split())
     arr = list(map(int,input().strip().split()))
     start = 0
-    s = arr[0]
+    s = 0
     flag = 0
-    for i in range(1,n+1):
+    for i in range(n):
+        s+=arr[i]
         while start<i and s>k:
             s-=arr[start]
             start+=1
         if s==k:
-            print(start+1,i)
+            print(start+1,i+1)
             flag = 1
             break
-        if i<n:
-            s+=arr[i]
+
     if flag==0:
         print(-1)
