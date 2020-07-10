ec3813e4ee7ef8c3a84c609370bb603515b2b72a
Ragul S V <41230960+RagulSV1110@users.noreply.github.com>
2020-07-10 21:14:18 +0530

    Create Count pairs with Bitwise XOR as odd number.py

---
+++ Bitwise Operations/Count pairs with Bitwise XOR as odd number.py
@@ -0,0 +1,17 @@
+def findOddPair(A, N) : 
+  
+    count = 0
+  
+    # find all pairs  
+    for i in range(0 , N) :  
+        if (A[i] % 2 == 0) : 
+            count+=1
+      
+    # return number of odd pair  
+    return count * (N - count)  
+  
+# Driver Code 
+if __name__=='__main__': 
+    a = [5, 4, 7, 2, 1]  
+    n = len(a) 
+    print(findOddPair(a,n)) 
