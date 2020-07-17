d0fdacc314e238fd8767385007e933371b5ef671
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-07-17 12:37:50 +0530

    Create Peak Element.py

--- Array/Easy/Peak Element.py
+++ Array/Easy/Peak Element.py
@@ -4,18 +4,12 @@ def peakElement(arr, n):
     res = 0
     while l<=r:
         mid = (l+r)//2
-        if mid-1>0 and mid+1<n-1 and arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]:
-            res = mid
-            break
-        elif (mid==0 or arr[mid]>=arr[mid-1]) and \
-            (mid==n-1 or arr[mid]>=arr[mid+1]):
-            res = mid
-            break
-        elif mid==n-1 and arr[mid]>arr[mid+1]:
+        if (mid==0 or arr[mid-1]<=arr[mid]) and (mid==n-1 or arr[mid+1]<=arr[mid]):
             res = mid
             break
         elif mid>0 and arr[mid-1]>arr[mid]:
             r = mid-1
         elif mid<n-1 and arr[mid+1]>arr[mid]:
             l = mid+1
+    print(arr[res])
     return res
