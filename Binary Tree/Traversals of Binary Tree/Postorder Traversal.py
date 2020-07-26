51171ad04df718351d20b7e2502c684cdff4af55
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-07-26 18:02:31 +0530

    Update Postorder Traversal.py

--- Binary Tree/Traversals of Binary Tree/Postorder Traversal.py
+++ Binary Tree/Traversals of Binary Tree/Postorder Traversal.py
@@ -1,5 +1,22 @@
 def postOrder(root):
+    
+    #Recursive
     if root:
         postorder(root.left)
         postorder(root.right)
         print(root.data,end=" ")
+        
+    #Iterative
+    if not root:
+        return
+    s1 = [root]
+    s2 = []
+    while s1:
+        cur = s1.pop()
+        s2.append(cur)
+        if cur.left:
+            s1.append(cur.left)
+        if cur.right:
+            s1.append(cur.right)
+    while s2:
+        print(s2.pop().data,end=" ")
