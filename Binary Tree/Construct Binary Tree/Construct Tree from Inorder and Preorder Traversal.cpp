c4d210bff7d4f393c99d555808da494fc6b9962f
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-07-26 12:32:20 +0530

    Update Construct Tree from Inorder and Preorder Traversal.cpp

--- Binary Tree/Construct Binary Tree/Construct Tree from Inorder and Preorder Traversal.cpp
+++ Binary Tree/Construct Binary Tree/Construct Tree from Inorder and Preorder Traversal.cpp
@@ -1,3 +1,4 @@
+#-------------------------------O(N^2)----------------------------------------------#
 Node* buildTree(int in[],int pre[], int l, int h)
 {
     if(l>h) return NULL;
@@ -15,3 +16,26 @@ Node* buildTree(int in[],int pre[], int l, int h)
     }
     return root;
 }
+
+/* Python using Hash Table to Store inorder elements index O(N)
+def build(inorder,d,preorder,preIndex,l,r):
+    if l>r:
+        return None
+    root = Node(preorder[preIndex[0]])
+    preIndex[0]+=1
+    if l==r:
+        return root
+    i = d[root.data]
+    if i<=r:
+        root.left = build(inorder,d,preorder,preIndex,l,i-1)
+        root.right = build(inorder,d,preorder,preIndex,i+1,r)
+    return root
+            
+    
+def buildtree(inorder, preorder, n):
+    preIndex = [0]
+    d = dict()
+    for i in range(n):
+        d[inorder[i]] = i
+    return build(inorder,d,preorder,preIndex,0,n-1)
+*/
