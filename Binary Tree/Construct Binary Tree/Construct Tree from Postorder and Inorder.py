eebce0c60d7dfb260ef268e9aae99803401d7058
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-07-26 12:41:52 +0530

    Update Construct Tree from Postorder and Inorder.py

--- Binary Tree/Construct Binary Tree/Construct Tree from Postorder and Inorder.py
+++ Binary Tree/Construct Binary Tree/Construct Tree from Postorder and Inorder.py
@@ -1,43 +1,43 @@
 #----------------------------------------------------------METHOD 1-----------------------------------------------------------------#
-def build(inorder,postorder,l,h,preindex):
+def build(inorder,d,postorder,l,h,postindex):
     if l>h:
         return None
-    root = Node(postorder[preindex[0]])
-    preindex[0]+=1
+    root = Node(postorder[postindex[0]])
+    postindex[0]+=1
     if l==h:
         return root
-    i = l 
-    for i in range(l,h+1):
-        if root.data==inorder[i]:
-            break
+    i = d[root.data]
     if i<=h:
-        root.right = build(inorder,postorder,i+1,h,preindex)
-        root.left = build(inorder,postorder,l,i-1,preindex)
+        root.right = build(inorder,d,postorder,i+1,h,postindex)
+        root.left = build(inorder,d,postorder,l,i-1,postindex)
     return root
         
 def buildTree(In, post, n):
     inorder = list(reversed(In))
     postorder = list(reversed(post))
-    preindex = [0]
-    return build(In,post,0,n-1,preindex)
+    postindex = [0]
+    d = dict()
+    for i in range(n):
+        d[In[i]] = i
+    return build(In,d,post,0,n-1,postindex)
     
 #---------------------------------------------------------METHOD 2-------------------------------------------------------------------#
-def build(inorder,postorder,l,h,preindex):
+def build(inorder,postorder,l,h,postindex):
     if l>h:
         return None
-    root = Node(postorder[preindex[0]])
+    root = Node(postorder[postindex[0]])
     preindex[0]-=1
     if l==h:
         return root
-    i = l
-    for i in range(l,h+1):
-        if root.data==inorder[i]:
-            break
+    i = d[root.data]
     if i<=h:
-        root.right = build(inorder,postorder,i+1,h,preindex)
-        root.left = build(inorder,postorder,l,i-1,preindex)
+        root.right = build(inorder,postorder,i+1,h,postindex)
+        root.left = build(inorder,postorder,l,i-1,postindex)
     return root
         
 def buildTree(In, post, n):
-    preindex = [n-1]
-    return build(In,post,0,n-1,preindex)
+    postindex = [n-1]
+    d = dict()
+    for i in range(n):
+        d[In[i]] = i
+    return build(In,post,0,n-1,postindex)
