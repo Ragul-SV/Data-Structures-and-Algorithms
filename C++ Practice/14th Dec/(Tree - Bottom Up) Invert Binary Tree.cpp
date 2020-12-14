49927e776bd9d46ec2d434b5d832923e5089c849
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-14 23:35:30 +0530

    Create (Tree - Bottom Up) Invert Binary Tree.cpp

---
+++ C++ Practice/14th Dec/(Tree - Bottom Up) Invert Binary Tree.cpp
@@ -0,0 +1,8 @@
+TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return NULL;
+        TreeNode* l = invertTree(root->left); 
+        TreeNode* r = invertTree(root->right); 
+        root->left = r;
+        root->right = l;
+        return root;
+    }
