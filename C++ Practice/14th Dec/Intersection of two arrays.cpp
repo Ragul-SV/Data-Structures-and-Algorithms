e3ba50733373eea5ee613d00d77980f7d713de38
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-14 08:25:08 +0530

    Create Intersection of two arrays.cpp

---
+++ C++ Practice/14th Dec/Intersection of two arrays.cpp
@@ -0,0 +1,13 @@
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(),nums1.end());
+        vector<int> res;
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(s.count(nums2[i]))
+            {
+                res.push_back(nums2[i]);
+                s.erase(nums2[i]);
+            }
+        }
+        return res;
+    }
