483e2a7b9792192c0673934d91f804a6f922324c
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-15 23:40:32 +0530

    Create (Linked List) Remove Linked List Elements.cpp

---
+++ C++ Practice/15th Dec/(Linked List) Remove Linked List Elements.cpp
@@ -0,0 +1,5 @@
+ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL) return NULL;
+        head->next = removeElements(head->next,val);
+        return head->val==val? head->next:head;
+    }
