605dba9efe7a7f83180f52b99adc80ce96d37d0f
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-15 23:50:59 +0530

    Create Swap Pairs in Pairs.cpp

---
+++ C++ Practice/15th Dec/Swap Pairs in Pairs.cpp
@@ -0,0 +1,8 @@
+ListNode* swapPairs(ListNode* head) {
+        if(head==NULL)return NULL;
+        if(head->next==NULL) return head;
+        ListNode* nxt = head->next;
+        head->next = swapPairs(nxt->next);
+        nxt->next = head;
+        return nxt;
+    }
