2ef9783d12923403e3ed4672db0a24f6c0731bf0
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-18 19:11:31 +0530

    Create (Tree) Minimum Depth of Binary Tree.cpp

---
+++ C++ Practice/18th Dec/(Tree) Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,6 @@
+int minDepth(TreeNode* root) {
+        if(root==NULL)return 0;
+        int l=minDepth(root->left);
+        int r=minDepth(root->right);
+        return (l==0 || r==0)? l+r+1 : min(l,r)+1; 
+    }
