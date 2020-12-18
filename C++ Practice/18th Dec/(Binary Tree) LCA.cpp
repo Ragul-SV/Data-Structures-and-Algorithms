8dab7a9ca68fc73c814229d2bf8122786a4388e5
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-18 08:06:50 +0530

    Create (Binary Tree) LCA.cpp

---
+++ C++ Practice/18th Dec/(Binary Tree) LCA.cpp
@@ -0,0 +1,10 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+        if(root->val==p->val || root->val==q->val)
+            return root;
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        if(l!=NULL && r!=NULL)return root;
+        if(l==NULL)return r;
+        else return l;
+    }
