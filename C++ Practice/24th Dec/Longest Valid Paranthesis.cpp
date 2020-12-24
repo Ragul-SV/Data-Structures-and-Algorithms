b5e9c0f9972e9c4c67fa5f5fec6a0076432a25f3
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-24 20:56:30 +0530

    Update Longest Valid Paranthesis.cpp

--- C++ Practice/24th Dec/Longest Valid Paranthesis.cpp
+++ C++ Practice/24th Dec/Longest Valid Paranthesis.cpp
@@ -1,3 +1,4 @@
+#------------------------------------------O(N)-TIME------O(N)-SPACE-------------------#
 int longestValidParentheses(string s) {
         int n = s.length();
         vector<int> dp(n,0);
@@ -12,6 +13,29 @@ int longestValidParentheses(string s) {
                     dp[i] = dp[i-1] + (i-dp[i-1]-2>=0?dp[i-dp[i-1]-2]:0) + 2;
                 res = max(res,dp[i]);
             }
+        }
+        return res;
+    }
+
+#------------------------------------------O(N)-TIME------O(1)-SPACE-------------------#
+int longestValidParentheses(string s) {
+        int n = s.length();
+        int res = 0;
+        int left=0,right=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')left++;
+            else right++;
+            if(left==right)res = max(res,left*2);
+            else if(right>left)left = right = 0;
+        }
+        left = right = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='(')left++;
+            else if(s[i]==')')right++;
+            if(left==right)res = max(res,left*2);
+            else if(left>right)left = right = 0;
         }
         return res;
     }
