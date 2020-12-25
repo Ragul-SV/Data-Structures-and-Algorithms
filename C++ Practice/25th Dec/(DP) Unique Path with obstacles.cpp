113a474d441e584278880c09af7bef325c7d478c
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-12-25 20:23:26 +0530

    Update (DP) Unique Path with obstacles.cpp

--- C++ Practice/25th Dec/(DP) Unique Path with obstacles.cpp
+++ C++ Practice/25th Dec/(DP) Unique Path with obstacles.cpp
@@ -2,34 +2,15 @@ int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         if(grid[0][0]==1)return 0;
-        for(int i = 0;i<m;i++)
+        grid[0][0] = 1;
+        for(int i=1;i<m;i++)
         {
-            if(grid[i][0]==1)
-            {
-                grid[i][0]=0;
-                while(i<m)
-                {
-                    if(grid[i][0]==1)grid[i][0] = 0;
-                    i++;
-                }
-                break;
-            }
-            grid[i][0] = 1;
+            grid[i][0] = int(grid[i][0]==0 && grid[i-1][0]==1);
         }
          
-        for(int j = 1;j<n;j++)
+        for(int j=1;j<n;j++)
         {
-            if(grid[0][j]==1)
-            {
-                grid[0][j]=0;
-                while(j<n)
-                {
-                    if(grid[0][j]==1)grid[0][j] = 0;
-                    j++;
-                }
-                break;
-            }
-            grid[0][j] = 1;
+            grid[0][j] = int(grid[0][j]==0 && grid[0][j-1]==1);
         }
        
         for(int i=1;i<m;i++)
@@ -37,8 +18,7 @@ int uniquePathsWithObstacles(vector<vector<int>>& grid) {
             for(int j=1;j<n;j++)
             {
                 if(grid[i][j]==1)grid[i][j] = 0;
-                else
-                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                else grid[i][j] = grid[i-1][j] + grid[i][j-1];
             }
         }
         
